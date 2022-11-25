fda43875d54234e6f2ab63d2bb5fb3394f61c568
Joao Braganca <joao.braganca@deus.ai>
2022-11-25 00:34:31 +0000

    create data file

--- createData.py
+++ createData.py
@@ -3,8 +3,6 @@ import csv
 # 1 a 319
 
 header = ['customer_email', 'customer_phone', 'status', 'corporate_id']
-data = ['Afghanistan', 652090, 'AF', 'AFG']
-
 
 with open('data.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
