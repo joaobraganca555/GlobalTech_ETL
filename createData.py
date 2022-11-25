25a4b0a3657ef93208988625de2e16675e50ebff
Joao Braganca <joao.braganca@deus.ai>
2022-11-25 01:17:25 +0000

    random for product_id

--- createData.py
+++ createData.py
@@ -2,9 +2,15 @@ import random
 import csv
 # 1 a 319
 
-header = ['customer_email', 'customer_phone', 'status', 'corporate_id']
+header = ['customer_email', 'customer_phone', 'status', 'employee_corporate_id', 'product_id']
+contacts = []
 
-with open('data.csv', 'w', encoding='UTF8', newline='') as f:
+with open('contacts.csv') as f:
+    for line in f:
+        line = line.strip().split(',')
+        contacts.append((line[3], line[4]))
+
+with open('sheet.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
 
     # write the header
@@ -12,13 +18,16 @@ with open('data.csv', 'w', encoding='UTF8', newline='') as f:
 
     i = 0
 
-    while i < 207:
+    while i < 5:
         # write the data
+        randomCustomer = random.randint(1, len(contacts))
         customerInfo = random.randint(1,3)
         if customerInfo == 1:
-            writer.writerow(['REFUNDED', random.randint(1,107)])
+            writer.writerow([contacts[randomCustomer][0], '','REFUNDED', random.randint(1,107), random.randint(1,107)])
         elif customerInfo == 2:
-            writer.writerow(['REFUNDED', random.randint(1,107)])
+            writer.writerow(['', contacts[randomCustomer][1],'REFUNDED', random.randint(1,107), random.randint(1,107)])
         else:
-            writer.writerow(['REFUNDED', random.randint(1,107)])
+            writer.writerow([contacts[randomCustomer][0], contacts[randomCustomer][1], 'REFUNDED', random.randint(1,107), random.randint(1,107)])
         i += 1
+
+        # VOU BUSCAR FONES
