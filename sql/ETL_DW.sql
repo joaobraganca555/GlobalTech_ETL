ccf34fd7b442aff7e1b972228ce7c99ded840625
Joao Braganca <joao.braganca@deus.ai>
2022-12-20 01:23:47 +0000

    minor fixes

--- sql/ETL_DW.sql
+++ sql/ETL_DW.sql
@@ -1,23 +1,4 @@
 USE master
-<<<<<<< HEAD
-GO
-
---Caso a database esteja em uso
-IF EXISTS (SELECT name
-FROM sys.databases
-WHERE name = N'DW_GlobalTech')
-ALTER DATABASE DW_GlobalTech SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-GO
-
-DROP DATABASE  IF EXISTS DW_GlobalTech
-GO
-
-CREATE DATABASE DW_GlobalTech
-GO
-
-USE DW_GlobalTech
-=======
->>>>>>> a7371c65b491693402b2d953ea5eed7a1d2b03af
 GO
 
 --Caso a database esteja em uso
